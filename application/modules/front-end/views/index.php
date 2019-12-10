ff7e74de6a8e38c9505d6b0030881e71e89255ed
BossPSR <boss3075030750@gmail.com>
2019-12-10 13:13:46 +0700

    update css index

--- application/modules/front-end/views/index.php
+++ application/modules/front-end/views/index.php
@@ -18,175 +18,104 @@
 					</div>
 				</div>
 			</div>
-			<span class="scoll-down">Scroll Down</span>
+			<!-- <span class="scoll-down">Scroll Down</span> -->
 		</section>
 		<!-- End Slide -->
-		
+
 		<!-- Section Special Offers -->
-		<section id="special-offers" class="padd-100">
+		<section id="special-offers" class="padd-100 stepFood">
 			<div class="container">
+
 				<div class="row">
-					<div class="col-md-6">
-						<div class="offer-left">
-							<span class="section-suptitle">From The Menu</span>
-							<h2 class="section-title">Special Offers</h2>
-							<div class="offer-item">
-								<img src="http://placehold.it/90x82" alt="" class="img-responsive">
-								<div>
-									<h3>Pring Veg & Pasta</h3>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-									</p>
-								</div>
-								<span class="offer-price">$10</span>
-							</div>
-							<div class="offer-item">
-								<img src="http://placehold.it/90x82" alt="" class="img-responsive">
-								<div>
-									<h3>Cheese /Bacon,Coleslaw</h3>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-									</p>
-								</div>
-								<span class="offer-price">$14</span>
-							</div>
-							<div class="offer-item">
-								<img src="http://placehold.it/90x82" alt="" class="img-responsive">
-								<div>
-									<h3>Croissant (Plain)</h3>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-									</p>
-								</div>
-								<span class="offer-price">$12</span>
-							</div>
-						</div>
-					</div>
-					<div class="col-md-6 hidden-sm hidden-xs">
-						<div class="offer-right">
-							<img src="http://placehold.it/550x550" alt="" class="img-responsive">
-							<a href="menu.html">
-								Explore
-								<span>The menu</span>
-							</a>
-						</div>
-					</div>
+					<h2 class="title_stepFood text-center">
+						อิ่มอร่อยถึงบ้านแค่ 3 ขั้นตอนง่ายๆ
+					</h2>
+					
 				</div>
+
+				<div class="row">
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-1">
+							
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-2">
+						<div class="how-it-works-wrap">
+							<div class="step">
+								<div class="icon" data-step="1">
+									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 483 483" width="512" height="512">
+                                            <g fill="#FFF">
+                                                <path d="M467.006 177.92c-.055-1.573-.469-3.321-1.233-4.755L407.006 62.877V10.5c0-5.799-4.701-10.5-10.5-10.5h-310c-5.799 0-10.5 4.701-10.5 10.5v52.375L17.228 173.164a10.476 10.476 0 0 0-1.22 4.938h-.014V472.5c0 5.799 4.701 10.5 10.5 10.5h430.012c5.799 0 10.5-4.701 10.5-10.5V177.92zM282.379 76l18.007 91.602H182.583L200.445 76h81.934zm19.391 112.602c-4.964 29.003-30.096 51.143-60.281 51.143-30.173 0-55.295-22.139-60.258-51.143H301.77zm143.331 0c-4.96 29.003-30.075 51.143-60.237 51.143-30.185 0-55.317-22.139-60.281-51.143h120.518zm-123.314-21L303.78 76h86.423l48.81 91.602H321.787zM97.006 55V21h289v34h-289zm-4.198 21h86.243l-17.863 91.602h-117.2L92.808 76zm65.582 112.602c-5.028 28.475-30.113 50.19-60.229 50.19s-55.201-21.715-60.23-50.19H158.39zM300 462H183V306h117v156zm21 0V295.5c0-5.799-4.701-10.5-10.5-10.5h-138c-5.799 0-10.5 4.701-10.5 10.5V462H36.994V232.743a82.558 82.558 0 0 0 3.101 3.255c15.485 15.344 36.106 23.794 58.065 23.794s42.58-8.45 58.065-23.794a81.625 81.625 0 0 0 13.525-17.672c14.067 25.281 40.944 42.418 71.737 42.418 30.752 0 57.597-17.081 71.688-42.294 14.091 25.213 40.936 42.294 71.688 42.294 24.262 0 46.092-10.645 61.143-27.528V462H321z"></path>
+                                                <path d="M202.494 386h22c5.799 0 10.5-4.701 10.5-10.5s-4.701-10.5-10.5-10.5h-22c-5.799 0-10.5 4.701-10.5 10.5s4.701 10.5 10.5 10.5z"></path> </g>
+                                    </svg>
+								</div>
+							</div>
+							<h3 style="color:#fff; text-align:center;">เลือกร้านอาหาร</h3>
+							<p style="color:#fff; text-align:center;">เรามีร้านอาหารออนไลน์มากกว่า 148 ร้านค้า</p>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-2">
+						<div class="arrowAll">
+							<div class="arrow-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+									width="100" height="100"
+									viewBox="0 0 172 172"
+									style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M95.7825,37.84c-1.69312,0.26875 -2.92937,1.73344 -2.9025,3.44v27.52h-75.68c-1.89469,0 -3.44,1.54531 -3.44,3.44v27.52c0,1.89469 1.54531,3.44 3.44,3.44h75.68v27.52c0.02688,1.29 0.76594,2.44563 1.92156,3.01c1.15562,0.56438 2.52625,0.44344 3.56094,-0.3225l58.48,-44.72c0.81969,-0.65844 1.29,-1.63937 1.29,-2.6875c0,-1.04812 -0.47031,-2.02906 -1.29,-2.6875l-58.48,-44.72c-0.72562,-0.59125 -1.65281,-0.86 -2.58,-0.7525zM99.76,48.16l49.45,37.84l-49.45,37.84v-24.08c0,-1.89469 -1.54531,-3.44 -3.44,-3.44h-75.68v-20.64h75.68c1.89469,0 3.44,-1.54531 3.44,-3.44z"></path></g></g></svg>
+							</div>
+						</div>
+					</div>
+
+					
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-2">
+						<div class="how-it-works-wrap">
+							<div class="step">
+								<div class="icon" data-step="2">
+								<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 380.721 380.721">
+                                        <g fill="#FFF">
+                                            <path d="M58.727 281.236c.32-5.217.657-10.457 1.319-15.709 1.261-12.525 3.974-25.05 6.733-37.296a543.51 543.51 0 0 1 5.449-17.997c2.463-5.729 4.868-11.433 7.25-17.01 5.438-10.898 11.491-21.07 18.724-29.593 1.737-2.19 3.427-4.328 5.095-6.46 1.912-1.894 3.805-3.747 5.676-5.588 3.863-3.509 7.221-7.273 11.107-10.091 7.686-5.711 14.529-11.137 21.477-14.506 6.698-3.724 12.455-6.982 17.631-8.812 10.125-4.084 15.883-6.141 15.883-6.141s-4.915 3.893-13.502 10.207c-4.449 2.917-9.114 7.488-14.721 12.147-5.803 4.461-11.107 10.84-17.358 16.992-3.149 3.114-5.588 7.064-8.551 10.684-1.452 1.83-2.928 3.712-4.427 5.6a1225.858 1225.858 0 0 1-3.84 6.286c-5.537 8.208-9.673 17.858-13.995 27.664-1.748 5.1-3.566 10.283-5.391 15.534a371.593 371.593 0 0 1-4.16 16.476c-2.266 11.271-4.502 22.761-5.438 34.612-.68 4.287-1.022 8.633-1.383 12.979 94 .023 166.775.069 268.589.069.337-4.462.534-8.97.534-13.536 0-85.746-62.509-156.352-142.875-165.705 5.17-4.869 8.436-11.758 8.436-19.433-.023-14.692-11.921-26.612-26.631-26.612-14.715 0-26.652 11.92-26.652 26.642 0 7.668 3.265 14.558 8.464 19.426-80.396 9.353-142.869 79.96-142.869 165.706 0 4.543.168 9.027.5 13.467 9.935-.002 19.526-.002 28.926-.002zM0 291.135h380.721v33.59H0z"></path> </g>
+                                    </svg>
+								</div>
+							</div>
+							<h3 style="color:#fff; text-align:center;">เลือกรายการอาหาร</h3>
+							<p style="color:#fff; text-align:center;">เรามีเมนูอาหารจากร้านอาหารออนไลน์มากกว่า 8257 รายการ</p>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-2">
+						<div class="arrowAll">
+							<div class="arrow-icon">
+								<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+									width="100" height="100"
+									viewBox="0 0 172 172"
+									style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#ffffff"><path d="M95.7825,37.84c-1.69312,0.26875 -2.92937,1.73344 -2.9025,3.44v27.52h-75.68c-1.89469,0 -3.44,1.54531 -3.44,3.44v27.52c0,1.89469 1.54531,3.44 3.44,3.44h75.68v27.52c0.02688,1.29 0.76594,2.44563 1.92156,3.01c1.15562,0.56438 2.52625,0.44344 3.56094,-0.3225l58.48,-44.72c0.81969,-0.65844 1.29,-1.63937 1.29,-2.6875c0,-1.04812 -0.47031,-2.02906 -1.29,-2.6875l-58.48,-44.72c-0.72562,-0.59125 -1.65281,-0.86 -2.58,-0.7525zM99.76,48.16l49.45,37.84l-49.45,37.84v-24.08c0,-1.89469 -1.54531,-3.44 -3.44,-3.44h-75.68v-20.64h75.68c1.89469,0 3.44,-1.54531 3.44,-3.44z"></path></g></g></svg>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-2">
+						<div class="how-it-works-wrap">
+							<div class="step">
+								<div class="icon" data-step="1">
+									<i class="fa fa-car" aria-hidden="true" style="font-size: 35px;color: #fff"></i>
+								</div>
+							</div>
+							<h3 style="color:#fff; text-align:center;">จัดส่งสินค้า</h3>
+							<p style="color:#fff; text-align:center;">บริการจัดส่งอาหารถึงบ้านคุณ</p>
+						</div>
+					</div>
+
+					<div class="col-xs-12 col-sm-12 col-md-4 col-lg-1">
+							
+					</div>
+
+				</div>
+				
 			</div>
 		</section>
 		<!-- End Section Special Offers -->
 		
-		<!-- Section Reservation -->
-		<section id="reservation" data-background="http://placehold.it/1902x1100" class="parallax-window">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-7">
-						<div class="form-reservation padd-100">
-							<span class="section-suptitle">
-								Your Table
-							</span>
-							<h2 class="section-title white-font">
-								Reservation Now!
-							</h2>
-							<form action="#" method="post">
-								<div class="column">
-									<span>
-										<input type="text" name="name" placeholder="Name">
-									</span>
-									<span>
-										<input type="text" name="contact_number" placeholder="Contact Number">
-									</span>
-								</div>
-								<div class="column">
-									<span>
-										<input type="text" name="email" placeholder="Email Adress">
-									</span>
-									<span>
-										<input type="text" name="num_person" placeholder="Number of Person">
-									</span>
-								</div>
-								<div class="column">
-									<span>
-										<input type="text" name="book_date" placeholder="Booke Date">
-										<i class="fa fa-calendar" aria-hidden="true"></i>
-									</span>
-									<span>
-										<input type="text" name="your_time" placeholder="Your Time ">
-										<i class="fa fa-clock-o" aria-hidden="true"></i>
-									</span>
-								</div>
-								<div class="column">
-									<textarea name="message" placeholder="Message of Request"></textarea>
-								</div>
-								<div class="submit-btn">
-									<button type="submit">Book Now</button>
-									<input type="hidden" class="num_table" value="">
-								</div>								
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End Section Reservation -->
-
-		<!-- Section Delas -->
-		<section id="delas" class="padd-100">
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-12">
-						<span class="section-suptitle text-center">Delas Today</span>
-						<h2 class="section-title sep-type-2 text-center">
-							Today Special Dish
-						</h2>
-						<div class="delas-carousel">
-							<div class="delas-item">
-								<img src="http://placehold.it/373x300" alt="" class="img-responsive">
-								<h4>Pring Veg & Pasta</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-								</p>
-								<span>Only $25</span>
-							</div>
-							<div class="delas-item">
-								<img src="http://placehold.it/373x300" alt="" class="img-responsive">
-								<h4>Pring Veg & Pasta</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-								</p>
-								<span>Only $25</span>
-							</div>
-							<div class="delas-item">
-								<img src="http://placehold.it/373x300" alt="" class="img-responsive">
-								<h4>Pring Veg & Pasta</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-								</p>
-								<span>Only $25</span>
-							</div>
-							<div class="delas-item">
-								<img src="http://placehold.it/373x300" alt="" class="img-responsive">
-								<h4>Pring Veg & Pasta</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-								</p>
-								<span>Only $25</span>
-							</div>
-							<div class="delas-item">
-								<img src="http://placehold.it/373x300" alt="" class="img-responsive">
-								<h4>Pring Veg & Pasta</h4>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur Vesti bulum vel ipsum ullamcorper.
-								</p>
-								<span>Only $25</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End Section Delas -->
-
+		
 		<!-- Section Restaurant Menu -->
 		<section id="restaurant-menu" class="padd-100">
 			<span class="section-suptitle text-center">Food Lover</span>
@@ -273,93 +202,22 @@
 			</div>
 		</section>
 		<!-- End Section Restaurant Menu -->
-
-		<!-- Section Testimonials -->
-		<section id="testimonials" class="padd-100 parallax-window" data-background="http://placehold.it/1902x1000">
-			<span class="section-suptitle text-center">
-				Food Lover
-			</span>
-			<h2 class="section-title white-font sep-type-2 text-center">
-				customer feedback
-			</h2>
-			<div class="container">
-				<div class="row">
-					<div class="col-sm-12 no-padd">
-						<ul class="testimonial-list">
-							<li>
-								<div>
-									<p>
-										Nunc ullamcorper augue nec accumsan
-										porta. Ut lacinia fgiat viverra. Ut dictum
-										turpis in ipsum sagittis finibus.
-									</p>
-									<span>Anna Van</span>
-								</div>
-								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
-							</li>
-							<li>
-								<div>
-									<p>
-										Nunc ullamcorper augue nec accumsan
-										porta. Ut lacinia fgiat viverra. Ut dictum
-										turpis in ipsum sagittis finibus.
-									</p>
-									<span>Frinton Van</span>
-								</div>
-								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
-							</li>
-							<li>
-								<div>
-									<p>
-										Nunc ullamcorper augue nec accumsan
-										porta. Ut lacinia fgiat viverra. Ut dictum
-										turpis in ipsum sagittis finibus.
-									</p>
-									<span>Filipe Van</span>
-								</div>
-								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
-							</li>
-							<li>
-								<div>
-									<p>
-										Nunc ullamcorper augue nec accumsan
-										porta. Ut lacinia fgiat viverra. Ut dictum
-										turpis in ipsum sagittis finibus.
-									</p>
-									<span>Frinton Van</span>
-								</div>
-								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
-							</li>
-							<li>
-								<div>
-									<p>
-										Nunc ullamcorper augue nec accumsan
-										porta. Ut lacinia fgiat viverra. Ut dictum
-										turpis in ipsum sagittis finibus.
-									</p>
-									<span>Frinton Van</span>
-								</div>
-								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
-							</li>
-						</ul>
-					</div>
-				</div>
-			</div>
-		</section>
-		<!-- End Section Testimonials -->
 		
+
 		<!-- Section Event -->
 		<section id="events" class="padd-100">
-			<span class="section-suptitle text-center">
-				Month of March
-			</span>
-			<h2 class="section-title sep-type-2 text-center">
-				upcoming event
-			</h2>
+			
+			
 			<div class="container">
+				
 				<div class="row">
 					<div class="col-sm-12  no-padd">
-						
+						<h2 class="promotionMonth text-center">
+							โปรโมชั่นของเดือนนี้
+						</h2>
+						<span class="section-suptitle promotionMonth-2">
+							Month of December
+						</span>
 						<ul class="event-carousel">
 							<li class="event-item">
 								<img src="http://placehold.it/370x247" alt="" class="img-responsive">
@@ -436,11 +294,8 @@
 
 		<!-- Section Teams -->
 		<section id="team" class="padd-100">
-			<span class="section-suptitle text-center">
-				Chef Team
-			</span>
 			<h2 class="section-title sep-type-2 text-center">
-				Best Chefs For You
+				อาหารแนะนำ
 			</h2>
 			<div class="container">
 				<div class="row">
@@ -536,6 +391,84 @@
 			</div>
 		</section>
 		<!-- End Section Teams -->
+
+		<!-- Section Testimonials -->
+		<section id="testimonials" class="padd-100 parallax-window" data-background="http://placehold.it/1902x1000">
+			<span class="section-suptitle text-center">
+				Food Lover
+			</span>
+			<h2 class="section-title white-font sep-type-2 text-center">
+				customer feedback
+			</h2>
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 no-padd">
+						<ul class="testimonial-list">
+							<li>
+								<div>
+									<p>
+										Nunc ullamcorper augue nec accumsan
+										porta. Ut lacinia fgiat viverra. Ut dictum
+										turpis in ipsum sagittis finibus.
+									</p>
+									<span>Anna Van</span>
+								</div>
+								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
+							</li>
+							<li>
+								<div>
+									<p>
+										Nunc ullamcorper augue nec accumsan
+										porta. Ut lacinia fgiat viverra. Ut dictum
+										turpis in ipsum sagittis finibus.
+									</p>
+									<span>Frinton Van</span>
+								</div>
+								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
+							</li>
+							<li>
+								<div>
+									<p>
+										Nunc ullamcorper augue nec accumsan
+										porta. Ut lacinia fgiat viverra. Ut dictum
+										turpis in ipsum sagittis finibus.
+									</p>
+									<span>Filipe Van</span>
+								</div>
+								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
+							</li>
+							<li>
+								<div>
+									<p>
+										Nunc ullamcorper augue nec accumsan
+										porta. Ut lacinia fgiat viverra. Ut dictum
+										turpis in ipsum sagittis finibus.
+									</p>
+									<span>Frinton Van</span>
+								</div>
+								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
+							</li>
+							<li>
+								<div>
+									<p>
+										Nunc ullamcorper augue nec accumsan
+										porta. Ut lacinia fgiat viverra. Ut dictum
+										turpis in ipsum sagittis finibus.
+									</p>
+									<span>Frinton Van</span>
+								</div>
+								<img src="http://placehold.it/95x95" alt="" class="img-responsive">
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- End Section Testimonials -->
+		
+		
+
+		
 
 		<!-- Section Vision -->
 		<section id="vision" data-background="http://placehold.it/1902x800" class="parallax-window padd-100">
